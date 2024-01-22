c647832fdc758026bb02fa5364575380a79bf1df
“dbrustolon” <diego.brustolon@hotmail.com>
2024-01-21 21:09:13 -0500

    :tada: initial commit

---
+++ .vscode/assets/script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.jpg")
+  } else {
+    //se  light mode, manter a imagem normal.
+    img.setAttribute("src", "./assets/avatar-dark1.jpg")
+  }
+
+  const alt = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute(
+      "alt",
+      "foto de cassia olhando para a esquerda, com oculos de sol e fone de ouvido branco"
+    )
+  } else {
+    img.setAttribute(
+      "alt",
+      "Foto de Cassia Nayara com roupa vermelha, levemente de perfil e sorrindo"
+    )
+  }
+}
