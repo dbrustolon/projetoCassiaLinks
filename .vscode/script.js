d114c584433edf113c1d641906527440dcafc80f
DiegoBrustolon <110419477+dbrustolon@users.noreply.github.com>
2024-01-22 21:44:01 -0500

    Update script.js
    to see what happens

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,24 +1,24 @@
-function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
+// function toggleMode() {
+//   const html = document.documentElement
+//   html.classList.toggle("light")
 
-  const img = document.querySelector("#profile img")
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.jpg")
-  } else {
-    img.setAttribute("src", "./assets/avatar-dark1.jpg")
-  }
+//   const img = document.querySelector("#profile img")
+//   if (html.classList.contains("light")) {
+//     img.setAttribute("src", "./assets/avatar-light.jpg")
+//   } else {
+//     img.setAttribute("src", "./assets/avatar-dark1.jpg")
+//   }
 
-  const alt = document.querySelector("#profile img")
-  if (html.classList.contains("light")) {
-    img.setAttribute(
-      "alt",
-      "foto de cassia olhando para a esquerda, com oculos de sol e fone de ouvido branco"
-    )
-  } else {
-    img.setAttribute(
-      "alt",
-      "Foto de Cassia Nayara com roupa vermelha, levemente de perfil e sorrindo"
-    )
-  }
-}
+//   const alt = document.querySelector("#profile img")
+//   if (html.classList.contains("light")) {
+//     img.setAttribute(
+//       "alt",
+//       "foto de cassia olhando para a esquerda, com oculos de sol e fone de ouvido branco"
+//     )
+//   } else {
+//     img.setAttribute(
+//       "alt",
+//       "Foto de Cassia Nayara com roupa vermelha, levemente de perfil e sorrindo"
+//     )
+//   }
+// }
